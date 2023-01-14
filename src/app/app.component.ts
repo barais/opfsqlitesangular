d1e9f3514520e443f5d88675da198050c6dc5dd1
Olivier Barais <olivieR.barais@gmail.com>
2023-01-14 18:14:48 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demoAngularSqlLite';
+
+  ngOnInit(): void {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./dbsqlite.worker', import.meta.url));
+      worker.onmessage = ({ data }) => {
+        console.error(data.payload.args)
+      };
+      worker.postMessage({msg:'load'});
+    } else {
+      // Web workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
 }
---
+++ src/app/dbsqlite.worker.ts
@@ -0,0 +1,103 @@
+/// <reference lib="webworker" />
+
+addEventListener('message', ( e ) => {
+  console.error(e);
+
+  switch (e.data.msg) {
+    case 'hello': {
+      const response = `worker response to ${e.data.msg}`;
+      postMessage({ msg: response });
+      break;
+    }
+  case 'load': {
+    // Import Webassembly script
+    //self.importScripts('./content/opencv/4/opencv.js')
+    const self1 = self as any;
+    self1['Module'] = {
+      scriptUrl: '/assets/sqlite/sqlite3.js',
+    };
+
+    //Load and await the .js OpenCV
+    self1.importScripts(self1['Module'].scriptUrl);
+    let db1 = new DB();
+
+    self1
+    .sqlite3InitModule({
+      print: db1.log,
+      printErr: db1.error,
+    })
+    .then(function (sqlite3: any) {
+      db1.log('Done initializing. Running demo...');
+      try {
+        db1.start(sqlite3);
+      } catch (e1:any) {
+        db1.error('Exception:', e1.message);
+      }
+    });
+
+    break;
+  }
+}
+
+});
+
+
+
+class DB {
+  db: any;
+  counter= 0;
+  logHtml (cssClass: string, ...args: any[]) {
+    postMessage({
+      type: 'log',
+      payload: { cssClass, args },
+    });
+  };
+  log(...args: (string | number)[]) {
+    this.logHtml('', ...args);
+
+  }
+  error (...args: string[]) { this.logHtml('error', ...args) }
+
+   start(sqlite3: any) {
+    const capi = sqlite3.capi; /*C-style API*/
+    const oo = sqlite3.oo1; /*high-level OO API*/
+    this.log('sqlite3 version', capi.sqlite3_libversion(), capi.sqlite3_sourceid());
+    if (sqlite3.opfs) {
+      console.error(sqlite3.opfs)
+      this.db = new sqlite3.oo1.OpfsDb('/mydb.sqlite3');
+      this.log('The OPFS is available.');
+    } else {
+      this.db = new oo.DB('/mydb.sqlite3', 'ct');
+      this.log('The OPFS is not available.');
+    }
+    this.log('transient db =', this.db.filename);
+
+    try {
+      this.log('Create a table...');
+      this.db.exec('CREATE TABLE IF NOT EXISTS t(a,b)');
+      this.log('Insert some data using exec()...');
+      let i;
+      for (i = 20; i <= 25; ++i) {
+        this.db.exec({
+          sql: 'INSERT INTO t(a,b) VALUES (?,?)',
+          bind: [i, i * 2],
+        });
+      }
+      this.log("Query data with exec() using rowMode 'array'...");
+      this.db.exec({
+        sql: 'SELECT a FROM t ORDER BY a LIMIT 3',
+        rowMode: 'array', // 'array' (default), 'object', or 'stmt'
+        callback: (row: any)=> {
+          this.log('row ', ++this.counter, '=', row);
+        },
+      });
+    } finally {
+      this.db.close();
+    }
+  };
+
+}
+
+
+
+
